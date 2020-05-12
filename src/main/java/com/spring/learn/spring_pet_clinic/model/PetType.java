bb64249be3d0b484ccb31c20e12c962b37f13368
vcsheel <viveksheel96@gmail.com>
2020-05-12 22:41:08 +0530

    Added Pet Clinic POJO's, closed #2

---
+++ src/main/java/com/spring/learn/spring_pet_clinic/model/PetType.java
@@ -0,0 +1,14 @@
+package com.spring.learn.spring_pet_clinic.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
