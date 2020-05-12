d14eba2b3a732a66e370a0dc05a1890482aae55a
vcsheel <viveksheel96@gmail.com>
2020-05-12 17:57:42 +0530

    Initial Project

---
+++ src/test/java/com/spring/learn/spring_pet_clinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.learn.spring_pet_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
