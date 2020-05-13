c2f2ba5db199a9af1ad773cc798eccfecb329313
vcsheel <viveksheel96@gmail.com>
2020-05-14 00:30:29 +0530

    List vets and owners, closed #15 and closed #16

--- pet-clinic-web/src/main/java/com/spring/learn/spring_pet_clinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/spring/learn/spring_pet_clinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package com.spring.learn.spring_pet_clinic.controllers;
 
+import com.spring.learn.spring_pet_clinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
