2e82b33fefe5d43aa6a9be09251511e66b587364
vcsheel <viveksheel96@gmail.com>
2020-05-14 21:05:11 +0530

    Added PetTypes data on bootstrap closes #31

--- pet-clinic-web/src/main/java/com/spring/learn/spring_pet_clinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/spring/learn/spring_pet_clinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.spring.learn.spring_pet_clinic.bootstrap;
 
 import com.spring.learn.spring_pet_clinic.model.Owner;
+import com.spring.learn.spring_pet_clinic.model.PetType;
 import com.spring.learn.spring_pet_clinic.model.Vet;
 import com.spring.learn.spring_pet_clinic.services.OwnerService;
+import com.spring.learn.spring_pet_clinic.services.PetTypeService;
 import com.spring.learn.spring_pet_clinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,10 +14,12 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
@@ -24,6 +28,7 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("============================================");
         System.out.println("Loading data...");
 
+        //adding owner data
         Owner owner1 = new Owner();
         owner1.setFirstName("John");
         owner1.setLastName("Snow");
@@ -38,6 +43,7 @@ public class DataLoader implements CommandLineRunner {
 
         System.out.println("Owners' data loaded");
 
+        //adding vet data
         Vet vet1 = new Vet();
         vet1.setFirstName("Robert");
         vet1.setLastName("Baratheon");
@@ -51,6 +57,17 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet2);
 
         System.out.println("Vets data loaded");
+
+        //adding pet Type data
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        petTypeService.save(cat);
+
+        System.out.println("PetType data loaded");
 
         System.out.println("Data loading finished..");
 
