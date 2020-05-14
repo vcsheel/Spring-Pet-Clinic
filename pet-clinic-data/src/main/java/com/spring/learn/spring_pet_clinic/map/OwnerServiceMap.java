ade6eeff128a42271dbc137339f77c4b24c18100
vcsheel <viveksheel96@gmail.com>
2020-05-14 21:57:19 +0530

    adder owner contact info and pets to owner closed #32

--- pet-clinic-data/src/main/java/com/spring/learn/spring_pet_clinic/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/spring/learn/spring_pet_clinic/map/OwnerServiceMap.java
@@ -1,13 +1,26 @@
 package com.spring.learn.spring_pet_clinic.map;
 
 import com.spring.learn.spring_pet_clinic.model.Owner;
+import com.spring.learn.spring_pet_clinic.model.Pet;
+import com.spring.learn.spring_pet_clinic.model.PetType;
 import com.spring.learn.spring_pet_clinic.services.OwnerService;
+import com.spring.learn.spring_pet_clinic.services.PetService;
+import com.spring.learn.spring_pet_clinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetService petService;
+    private final PetTypeService petTypeService;
+
+    public OwnerServiceMap(PetService petService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.petTypeService = petTypeService;
+    }
+
     @Override
     public Owner findById(Long id) {
         return super.findById(id);
@@ -15,7 +28,33 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+
+        if(object != null) {
+
+            //save pet and petType to database if they are not saved into database
+            if(object.getPets() != null) {
+                object.getPets().forEach(pet -> {
+
+                    if(pet.getPetType() != null) {
+                        if(pet.getPetType().getId() == null) {
+                            petTypeService.save(pet.getPetType());
+                        }
+                    } else {
+                        throw new RuntimeException("Pet Type is required");
+                    }
+
+                    if(pet.getId() == null) {
+                        petService.save(pet);
+                        System.out.println("Saved pet into db: "+pet.getName());
+                    }
+                });
+            }
+
+            return super.save(object);
+
+        } else {
+            return null;
+        }
     }
 
     @Override
--- pet-clinic-web/src/main/java/com/spring/learn/spring_pet_clinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/spring/learn/spring_pet_clinic/bootstrap/DataLoader.java
@@ -1,13 +1,17 @@
 package com.spring.learn.spring_pet_clinic.bootstrap;
 
 import com.spring.learn.spring_pet_clinic.model.Owner;
+import com.spring.learn.spring_pet_clinic.model.Pet;
 import com.spring.learn.spring_pet_clinic.model.PetType;
 import com.spring.learn.spring_pet_clinic.model.Vet;
 import com.spring.learn.spring_pet_clinic.services.OwnerService;
+import com.spring.learn.spring_pet_clinic.services.PetService;
 import com.spring.learn.spring_pet_clinic.services.PetTypeService;
 import com.spring.learn.spring_pet_clinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -15,11 +19,13 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private final PetService petService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, PetService petService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.petService = petService;
     }
 
     @Override
@@ -28,16 +34,48 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("============================================");
         System.out.println("Loading data...");
 
+        //adding pet Type data
+        PetType dragon = new PetType();
+        dragon.setName("Dragon");
+        PetType savedDragon = petTypeService.save(dragon);
+
+        PetType wolf = new PetType();
+        wolf.setName("Direwolf");
+        PetType direwolf = petTypeService.save(wolf);
+
+        System.out.println("PetType data loaded");
+
         //adding owner data
         Owner owner1 = new Owner();
-        owner1.setFirstName("John");
+        owner1.setFirstName("Jon");
         owner1.setLastName("Snow");
+        owner1.setAddress("Winterfell");
+        owner1.setCity("The Wall");
+        owner1.setTelephone("1234567890");
+        Pet ghost = new Pet();
+        ghost.setName("Ghost");
+        ghost.setBirthDate(LocalDate.now());
+        ghost.setPetType(direwolf);
+        ghost.setOwner(owner1);
+        petService.save(ghost);
+        owner1.getPets().add(ghost);
 
         ownerService.save(owner1);
 
+        System.out.println("Owner1 done");
+
         Owner owner2 = new Owner();
-        owner2.setFirstName("Danny");
-        owner2.setLastName("Targereon");
+        owner2.setFirstName("Daenerys");
+        owner2.setLastName("Targaryen");
+        owner2.setAddress("Westeros");
+        owner2.setCity("Dragonstone");
+        owner2.setTelephone("9876543210");
+        Pet rhaegal = new Pet();
+        rhaegal.setOwner(owner2);
+        rhaegal.setName("Rhaegal");
+        rhaegal.setPetType(savedDragon);
+        rhaegal.setBirthDate(LocalDate.now());
+        owner2.getPets().add(rhaegal);
 
         ownerService.save(owner2);
 
@@ -57,17 +95,6 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet2);
 
         System.out.println("Vets data loaded");
-
-        //adding pet Type data
-        PetType dog = new PetType();
-        dog.setName("Dog");
-        petTypeService.save(dog);
-
-        PetType cat = new PetType();
-        cat.setName("Cat");
-        petTypeService.save(cat);
-
-        System.out.println("PetType data loaded");
 
         System.out.println("Data loading finished..");
 
