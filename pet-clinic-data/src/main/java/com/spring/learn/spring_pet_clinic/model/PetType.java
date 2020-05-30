0990eafbbdaa96371dbbdefeb17d2f36470fcbb8
vcsheel <viveksheel96@gmail.com>
2020-05-30 13:13:01 +0530

    Add new pet, #56

--- pet-clinic-data/src/main/java/com/spring/learn/spring_pet_clinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/spring/learn/spring_pet_clinic/model/PetType.java
@@ -10,7 +10,6 @@ import javax.persistence.Table;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
@@ -21,5 +20,11 @@ public class PetType extends BaseEntity {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
     }
 }
