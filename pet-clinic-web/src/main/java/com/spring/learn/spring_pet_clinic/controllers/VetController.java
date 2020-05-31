5cd72e0cf3efa40f725d90968789215e7fc58ddc
vcsheel <viveksheel96@gmail.com>
2020-05-31 21:37:05 +0530

    Added vet list with specialities and JSON response

--- pet-clinic-web/src/main/java/com/spring/learn/spring_pet_clinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/spring/learn/spring_pet_clinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.spring.learn.spring_pet_clinic.controllers;
 
+import com.spring.learn.spring_pet_clinic.model.Vet;
 import com.spring.learn.spring_pet_clinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @RequestMapping("/vets")
 @Controller
@@ -19,5 +24,10 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getAllVetsJSON() {
+        return vetService.findAll();
     }
 }
